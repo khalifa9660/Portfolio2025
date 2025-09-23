88473873a7a510c98b9ce39185a0b597d8b4fafc
khali <asura.9660@gmail.com>
2025-09-23 21:07:35 +0200

    Ajout d'animation pour text, a, i et ajout animation developer & web developer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Portfolio2025';
+
+  ngAfterViewInit() {
+    const options = {
+      strings: ['Fronted Developer', 'Web Developer'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    };
+    new Typed('.text', options);
+  }
 }
